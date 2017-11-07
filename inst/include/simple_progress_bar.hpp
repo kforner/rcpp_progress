6a2c98abe9d8c25c2fa5da3e69f7b8aa91f72e5b
nevrome <clemens@nevrome.de>
2017-11-07 19:45:18 +0100

    code layout

--- inst/include/simple_progress_bar.hpp
+++ inst/include/simple_progress_bar.hpp
@@ -31,69 +31,69 @@ class SimpleProgressBar: public ProgressBar{
     ~SimpleProgressBar() {
     }
     
-    public: // ===== main methods =====
-      
-      void display_progress_bar() {
-        REprintf("0%%   10   20   30   40   50   60   70   80   90   100%%\n");
-        REprintf("|----|----|----|----|----|----|----|----|----|----|\n");
-        flush_console();
+  public: // ===== main methods =====
+    
+    void display_progress_bar() {
+      REprintf("0%%   10   20   30   40   50   60   70   80   90   100%%\n");
+      REprintf("|----|----|----|----|----|----|----|----|----|----|\n");
+      flush_console();
+    }
+    
+    // will finalize display if needed
+    void update(float progress) {
+      _update_ticks_display(progress);
+      if (_ticks_displayed >= _max_ticks)
+        _finalize_display();
+    }
+    
+    void end_display() {
+      update(1);
+    }
+    
+    
+  protected: // ==== other instance methods =====
+    
+    // update the ticks display corresponding to progress
+    void _update_ticks_display(float progress) {
+      int nb_ticks = _compute_nb_ticks(progress);
+      int delta = nb_ticks - _ticks_displayed;
+      if (delta > 0) {
+        _display_ticks(delta);
+        _ticks_displayed = nb_ticks;
       }
       
-      // will finalize display if needed
-      void update(float progress) {
-        _update_ticks_display(progress);
-        if (_ticks_displayed >= _max_ticks)
-          _finalize_display();
+    }
+    
+    void _finalize_display() {
+      if (_finalized) return;
+      
+      REprintf("|\n");
+      flush_console();
+      _finalized = true;
+    }
+    
+    int _compute_nb_ticks(float progress) {
+      return int(progress * _max_ticks);
+    }
+    
+    void _display_ticks(int nb) {
+      for (int i = 0; i < nb; ++i) {
+        REprintf("*");
+        R_FlushConsole();
       }
-      
-      void end_display() {
-        update(1);
-      }
-      
-      
-      protected: // ==== other instance methods =====
-        
-        // update the ticks display corresponding to progress
-        void _update_ticks_display(float progress) {
-          int nb_ticks = _compute_nb_ticks(progress);
-          int delta = nb_ticks - _ticks_displayed;
-          if (delta > 0) {
-            _display_ticks(delta);
-            _ticks_displayed = nb_ticks;
-          }
-          
-        }
-        
-        void _finalize_display() {
-          if (_finalized) return;
-          
-          REprintf("|\n");
-          flush_console();
-          _finalized = true;
-        }
-        
-        int _compute_nb_ticks(float progress) {
-          return int(progress * _max_ticks);
-        }
-        
-        void _display_ticks(int nb) {
-          for (int i = 0; i < nb; ++i) {
-            REprintf("*");
-            R_FlushConsole();
-          }
-        }
-        
-        // N.B: does nothing on windows
-        void flush_console() {
-          #if !defined(WIN32) && !defined(__WIN32) && !defined(__WIN32__)
-                    R_FlushConsole();
-          #endif
-        }
-        
-        private:
-          int _max_ticks;   		// the total number of ticks to print
-          int _ticks_displayed; 	// the nb of ticks already displayed
-          bool _finalized;
+    }
+    
+    // N.B: does nothing on windows
+    void flush_console() {
+      #if !defined(WIN32) && !defined(__WIN32) && !defined(__WIN32__)
+                R_FlushConsole();
+      #endif
+    }
+    
+  private:
+    int _max_ticks;   		// the total number of ticks to print
+    int _ticks_displayed; 	// the nb of ticks already displayed
+    bool _finalized;
         
 };
 
