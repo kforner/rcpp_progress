7540a2fc34b7ebc69b750ee0d33fa91cd664007a
Karl Forner <karl.forner@quartz.bio>
2017-11-14 15:33:42 +0100

    added ... params to test functions, passed to load_all() to e.g. for recompilation in the example, when the example is run on successive different architectures.

--- R/wrap_examples.R
+++ R/wrap_examples.R
@@ -1,36 +1,36 @@
-load_my_example_pkg <- function(pkg) {
+load_my_example_pkg <- function(pkg, ...) {
   path <- system.file(file.path('examples', pkg), package = 'RcppProgress')
-  load_all(path, quiet = TRUE)
+  load_all(path, quiet = TRUE, ...)
 }
 
 get_function_from_pkg <- function(pkg, fun) {
   get(fun, getNamespace(pkg))
 }
 
-test_sequential <- function(max = 100, nb = 1000, display_progress= TRUE) {
+test_sequential <- function(max = 100, nb = 1000, display_progress= TRUE, ...) {
   pkg <- 'RcppProgressExample'
-  load_my_example_pkg(pkg)
+  load_my_example_pkg(pkg, ...)
   fun <- get_function_from_pkg(pkg, 'test_sequential')
   fun(max, nb, display_progress)
 }
 
 # R wrapper for the example function #2
 test_multithreaded <- function(max = 100, nb = 1000, threads = 0,
-    display_progress = TRUE)
+    display_progress = TRUE, ...)
 {
   pkg <- 'RcppProgressExample'
-  load_my_example_pkg(pkg)
+  load_my_example_pkg(pkg, ...)
   fun <- get_function_from_pkg(pkg, 'test_multithreaded')
   fun(max, nb, threads, display_progress)
 }
 
 
 amardillo_multithreaded <- function(max = 100, nb = 1000, threads = 0,
-  display_progress = TRUE)
+  display_progress = TRUE, ...)
 {
   testthat::skip_if_not_installed('RcppArmadillo')
   pkg <- 'RcppProgressArmadillo'
-  load_my_example_pkg(pkg)
+  load_my_example_pkg(pkg, ...)
   fun <- get_function_from_pkg(pkg, 'test_multithreaded')
   fun(max, nb, threads, display_progress)
 }
