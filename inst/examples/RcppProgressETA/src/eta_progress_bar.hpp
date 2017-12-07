df48e1b4d5b5abd26a2347703d31363ea6758f34
nevrome <clemens@nevrome.de>
2017-12-07 16:26:08 +0100

    eta progress bar with console feed update

--- inst/examples/RcppProgressETA/src/eta_progress_bar.hpp
+++ inst/examples/RcppProgressETA/src/eta_progress_bar.hpp
@@ -19,141 +19,168 @@
 
 // for unices only
 #if !defined(WIN32) && !defined(__WIN32) && !defined(__WIN32__)
-#include <Rinterface.h>
+  #include <Rinterface.h>
 #endif
 
 class ETAProgressBar: public ProgressBar{
-public: // ====== LIFECYCLE =====
-
-	/**
-	 * Main constructor
-	 */
-	ETAProgressBar()  {
-		_max_ticks = 50;
-		_ticks_displayed = 0;
-		_finalized = false;
-		_mode_flag = 1;
-		_timer_flag = true;
-		_timer_count = 1;
-	}
-
-	~ETAProgressBar() {
-	}
-
-public: // ===== main methods =====
-
-	void display() {
-		REprintf("0%% 10%%\n");
-		REprintf("|----|\n");
-		flush_console();
-	}
-
-	// will finalize display if needed
-	void update(float progress) {
-
-    _update_ticks_display(progress);
-    if (_ticks_displayed >= _max_ticks)
-      _finalize_display();
-
-		if (_timer_flag) {
-		  _timer_flag = false;
-		  time(&start);
-		} else if (_ticks_displayed >= _max_ticks) {
-		  time(&end);
-		} else if ((int) (((double)_ticks_displayed / (double)_max_ticks) * 100) == 10 * _timer_count) {
-
-      _timer_count++;
-
-		  time(&end);
-
-		  double pas_time = std::difftime(end, start);
-		  double rem_time = (pas_time / progress) * (1 - progress);
-		  int pas_time_simple = (int) pas_time;
-      int rem_time_simple = (int) rem_time;
-
-      std::stringstream rem_strs;
-      if (rem_time_simple > 3600) {
-        rem_strs << (int) (rem_time_simple / 3600) << "h";
-      } else if (rem_time_simple > 60) {
-        rem_strs << (int) (rem_time_simple / 60) << "min";
-      } else {
-        rem_strs << rem_time_simple << "s";
+  public: // ====== LIFECYCLE =====
+    
+    /**
+    * Main constructor
+    */
+    ETAProgressBar()  {
+      _max_ticks = 50;
+      _ticks_displayed = 0;
+      _finalized = false;
+      _mode_flag = 1;
+      _timer_flag = true;
+    }
+    
+    ~ETAProgressBar() {
+    }
+    
+    public: // ===== main methods =====
+      
+      void display() {
+        REprintf("0%%   10   20   30   40   50   60   70   80   90   100%%\n");
+        REprintf("[----|----|----|----|----|----|----|----|----|----|\n");
+        flush_console();
       }
-      std::string rem = rem_strs.str();
-
-      std::stringstream pas_strs;
-      if (pas_time_simple > 3600) {
-        pas_strs << (int) (pas_time_simple / 3600) << "h";
-      } else if (pas_time_simple > 60) {
-        pas_strs << (int) (pas_time_simple / 60) << "min";
-      } else {
-        pas_strs << pas_time_simple << "s";
+      
+      // will finalize display if needed
+      void update(float progress) {
+        
+        // condition to manage time measurement
+        if (_timer_flag) {
+          _timer_flag = false;
+          time(&start);
+        } else if ((progress > 0.001) & !_finalized) {
+          
+          time(&end);
+          
+          // calculate passed time and remaining time (in seconds)
+          double pas_time = std::difftime(end, start);
+          double rem_time = (pas_time / progress) * (1 - progress);
+          
+          // convert seconds to time string
+          std::string time_string = _time_to_string(rem_time);
+          
+          // create progress bar string 
+          std::string progress_bar_string = _current_ticks_display(progress);
+          
+          // ensure overwriting of old time info
+          int empty_length = time_string.length();
+          std::string empty_space = std::string(empty_length, ' ');
+          
+          // merge progress bar and time string
+          std::stringstream strs;
+          strs << "|" << progress_bar_string << "| " << time_string << empty_space;
+          std::string temp_str = strs.str();
+          char const* char_type = temp_str.c_str();
+          
+          // print: remove old and replace with new
+          REprintf("\r");
+          REprintf("%s", char_type);
+          REprintf("\r");
+          
+          #if !defined(WIN32) && !defined(__WIN32) && !defined(__WIN32__)
+            R_FlushConsole();
+          #endif
+          R_CheckUserInterrupt();
+          
+          // finalize display when ready
+          if(progress == 1) {
+            _finalize_display();
+          }  
+        }
       }
-      std::string pas = pas_strs.str();
-
-		  std::stringstream strs;
-		  strs << "| " << rem << " (" << pas << ")\n";
-		  std::string temp_str = strs.str();
-		  char const* char_type = temp_str.c_str();
-
-		  REprintf(char_type);
-
-		}
-	}
-
-	void end_display() {
-		update(1);
-	}
-
-
-  protected: // ==== other instance methods =====
-
-  	// update the ticks display corresponding to progress
-  	void _update_ticks_display(float progress) {
-  		int nb_ticks = _compute_nb_ticks(progress);
-  		int delta = nb_ticks - _ticks_displayed;
-  		if (delta > 0) {
-  			_display_ticks(delta);
-  			_ticks_displayed = nb_ticks;
-  		}
-
-  	}
-
-  	void _finalize_display() {
-  		if (_finalized) return;
-
-  		REprintf("|\n");
-      flush_console();
-  		_finalized = true;
-  	}
-
-  	int _compute_nb_ticks(float progress) {
-  		return int(progress * _max_ticks);
-  	}
-
-  	void _display_ticks(int nb) {
-  		for (int i = 0; i < nb; ++i) {
-  			REprintf("*");
-  			R_FlushConsole();
-  		}
-  	}
-
-  	// N.B: does nothing on windows
-  	void flush_console() {
-  #if !defined(WIN32) && !defined(__WIN32) && !defined(__WIN32__)
-  	  R_FlushConsole();
-  #endif
-  	}
-
-  private: // ===== INSTANCE VARIABLES ====
-  	int _max_ticks;   		// the total number of ticks to print
-  	int _ticks_displayed; 	// the nb of ticks already displayed
-  	bool _finalized;
-  	int _mode_flag;
-    bool _timer_flag;
-    time_t start,end;
-    int _timer_count;
-
+      
+      void end_display() {
+        update(1);
+      }
+      
+      
+      protected: // ==== other instance methods =====
+        
+        // convert double with seconds to time string
+        std::string _time_to_string(double seconds) {
+          
+          int time = (int) seconds;
+          
+          int hour = 0;
+          int min = 0;
+          int sec = 0;
+          
+          hour = time / 3600;
+          time = time % 3600;
+          min = time / 60;
+          time = time % 60;
+          sec = time;
+          
+          std::stringstream time_strs;
+          if (hour != 0) time_strs << hour << "h ";
+          if (min != 0) time_strs << min << "min ";
+          if (sec != 0) time_strs << sec << "s ";
+          std::string time_str = time_strs.str();
+          
+          return time_str;
+        }
+        
+        // update the ticks display corresponding to progress
+        std::string _current_ticks_display(float progress) {
+          
+          int nb_ticks = _compute_nb_ticks(progress);
+          
+          std::string cur_display = _construct_ticks_display_string(nb_ticks);
+          
+          return cur_display;
+        }
+        
+        // construct progress bar display
+        std::string _construct_ticks_display_string(int nb) {
+          
+          std::stringstream ticks_strs;
+          for (int i = 0; i < (_max_ticks - 1); ++i) {
+            if (i < nb) {
+              ticks_strs << "*";
+            } else {
+              ticks_strs << " ";
+            }
+          }
+          std::string tick_space_string = ticks_strs.str();
+          
+          return tick_space_string;
+        }
+        
+        // finalize
+        void _finalize_display() {
+          if (_finalized) return;
+          
+          REprintf("\n");
+          flush_console();
+          _finalized = true;
+        }
+        
+        // compute number of ticks according to progress
+        int _compute_nb_ticks(float progress) {
+          return int(progress * _max_ticks);
+        }
+        
+        // N.B: does nothing on windows
+        void flush_console() {
+          #if !defined(WIN32) && !defined(__WIN32) && !defined(__WIN32__)
+            R_FlushConsole();
+          #endif
+        }
+        
+        private: // ===== INSTANCE VARIABLES ====
+          int _max_ticks;   		// the total number of ticks to print
+          int _ticks_displayed; 	// the nb of ticks already displayed
+          bool _finalized;
+          int _mode_flag;
+          bool _timer_flag;
+          time_t start,end;
+          
 };
 
 #endif
